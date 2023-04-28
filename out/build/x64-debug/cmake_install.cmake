b9387af706f9edb3e70177668ff46377b4eb48ea
turbotage <viktor.vigren@gmail.com>
2023-04-28 11:34:00 +0200

    removed out folder

--- out/build/x64-debug/cmake_install.cmake
+++ out/build/x64-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Documents/GitHub/LibTorch20
+# Install script for directory: D:/GitHub/LibTorch20
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/Documents/GitHub/LibTorch20/out/install/x64-debug")
+  set(CMAKE_INSTALL_PREFIX "D:/GitHub/LibTorch20/out/install/x64-debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Documents/GitHub/LibTorch20/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/GitHub/LibTorch20/out/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
